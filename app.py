1d88654ceed03466c74acfbede6ea87f791231b9
Anmol Kankarwal <anmolkankarwal@gmail.com>
2026-05-06 16:28:01 +0530

    Configured app for Render deployment

--- app.py
+++ app.py
@@ -116,5 +116,5 @@ def disclaimer():
     return render_template('disclaimer.html')
 
 # === Run ===
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
